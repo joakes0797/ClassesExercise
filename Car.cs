ff35224c3ad20679ecabfcacd28695edf4f9ffd6
Jill Oakes <jericho_rules@hotmail.com>
2022-03-30 11:10:37 -0400

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car
+            {
+                Make = "Toyota",
+                Model = "Camry",
+                Year = 2014
+            };
+
+            Console.WriteLine($"Would you like to drive a {myCar.Year} {myCar.Make} {myCar.Model}?  It's a decent car.");
+        }
+    }
+}
